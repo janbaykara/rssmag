0aabd26c9ae9912a682c595ae7f2c7d0b5df2558
Jan Baykara <jan@baykara.co.uk>
2017-10-05 19:30:57 -0300

    Axios for API calls + server-memory caching

--- server/www.js
+++ server/www.js
@@ -1,12 +1,35 @@
-import Koa from 'koa';
-import koaStatic from 'koa-static';
-import KoaRouter from 'koa-router';
+import dotenv from 'dotenv'
+import Koa from 'koa'
+import koaStatic from 'koa-static'
+import KoaRouter from 'koa-router'
+import axios from 'axios'
+import cachios from 'cachios'
 
-const app = new Koa();
-const router =  new KoaRouter();
+dotenv.config()
 
-// 1. User auth with Feedly
+const app = new Koa()
+const router =  new KoaRouter()
+
+// 1. Feedly OAuth2
+//- Using dev token for now
+// consider https://github.com/hildjj/node-feedly ?
+let feedlyAPI = cachios.create(axios.create({
+	baseURL: 'http://cloud.feedly.com/v3',
+  headers: {
+		'Authorization': `OAuth ${process.env.feedlyAccessToken}`
+	}
+}), { // Infinite cache
+  stdTTL: 0,
+  checkperiod: 0 // ms cron for cache deletion
+})
+
 // 2. Fetch Feedly data
+// DOCS: https://developer.feedly.com/v3/sandbox/
+feedlyAPI.get('/categories').then((x) => {
+	console.log(`API calls today: ${x.headers['x-ratelimit-count']}`)
+	console.log(x.data)
+})
+
 // 3. Categorise, bundle, format
 // 4. Send to UI for rendering
 router.get('/api/', async (ctx) => {
@@ -14,14 +37,14 @@ router.get('/api/', async (ctx) => {
 		status: 'online',
 		resources: {}
 	}
-});
+})
 
-app.use(router.routes());
+app.use(router.routes())
 // create-react-app in future
-app.use(koaStatic('./build'));
+app.use(koaStatic('./build'))
 
-const port = 3000;
+const port = 3000
 
 app.listen(port, () => {
-	console.log(`Service started on port ${port}`);
-});
+	console.log(`Service started on port ${port}`)
+})

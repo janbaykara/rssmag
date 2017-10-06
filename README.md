ca5c5ec3f7c0e2c71720a0f87d2f2deb9b65de2a
Jan Baykara <jan@baykara.co.uk>
2017-10-06 12:14:34 -0300

    Simple (ineffective) grouping by tags

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
 # RSSMAG
 A more natural, magazine-like reading experience for RSS.
 
-Inspired by the history of print media and [Google News](news.google.com).
+Inspired by the history of print media, [Google News](news.google.com) and [Flipboard](flipboard.com).
+
+Hopefully we can solve [this](https://news.ycombinator.com/item?id=12933006).
 
 #### Wishlist
-* [ ] Integrates with [Feedly](feedly.com)
+* [X] Integrates with [Feedly](feedly.com)
 * [ ] Auto-groups stories by broad theme
 * [ ] Bundles reporting on specific issues
 * [ ] Responsive magazine layout for stories
--- server/www.js
+++ server/www.js
@@ -7,6 +7,7 @@ import { setupCache } from 'axios-cache-adapter-node'
 import nodeJsonDriver from './nodeJsonDriver'
 import htmlToText from 'html-to-text'
 import autoTagger from 'auto-tagger'
+import levenshtein from 'fast-levenshtein'
 
 dotenv.config()
 
@@ -42,20 +43,78 @@ feedlyAPI
 	feedlyAPI
 	.get(`streams/${encodeURIComponent(streamId)}/contents`)
 	.then(res => {
+		let articles = res.data.items
+
 		// console.log(`API access number __${res.headers['x-ratelimit-count']}__ - ${res.request.fromCache ? 'CACHED' : 'new data'}`)
-		console.log(`No of Entries: ${res.data.items.length}`)
+		console.log(`No of Entries: ${articles.length}`)
 
 		// Tag articles
 		// Ultimately I want a venn diagram of most-exclusive tags
-		var tags = autoTagger
+		var tagger = autoTagger
 			.useStopWords('en')
-			.useStopWords(['http','https','www','com','co','org'])
+			.useStopWords(['the','and'
+				// Technical
+				,'http','https','www','com','co','org','php','spip'
+				// Services
+				,'feedburner', 'rss',
+				// Syntax
+				,'–',':','/',',','.','','–'
+				// Datetime
+				,'2017','2016','2015','2014',
+				,'January','February','March','April','May','June','July','August','September','October','November','December'
+				// Things that should be auto-filtered
+				,'internationalviewpoint'
+			])
 
-		res.data.items.forEach(item => {
-			let textBlob = item.title+/*item.keywords+*/(/*item.content ? item.content.content :*/ item.summary ? item.summary.content : '')
+		let categoryBlob = ''
+
+		articles.map(article => {
+			let textBlob =
+				article.title
+				+article.keywords
+				+(
+					// article.content ? article.content.content :
+					article.summary ? article.summary.content : ''
+				)
+			textBlob = textBlob.replace(new RegExp(article.origin.title,'i'), '')
+
 			textBlob = htmlToText.fromString(textBlob);
-			console.log(tags.fromText(textBlob,3,3))
+			categoryBlob += textBlob
+			article.tagData = tagger.fromText(textBlob,3,3)
+			article.tags = article.tagData.map(tag => tag.word)
+			article.tags.concat(article.keywords)
+
+			return article
 		})
+
+		let categoryTags = tagger.fromText(categoryBlob,7,3)
+		// Focus on unusual words (e.g. by low->high corpus frequency)
+		// Group tags by overlap
+
+		let bundle = {}
+
+		categoryTags.map(t=>t.word).forEach(tag => {
+			bundle[tag] = []
+			articles.forEach(article => {
+				if(article.tags.includes(tag) && !article.bundle) {
+					article.bundle = tag
+					bundle[tag].push(article.title)
+				} else if(article.bundle) {
+					// console.log(`Article is bundled in ${article.bundle}: ${article.title}`)
+				}
+			})
+		})
+
+		console.log(bundle)
+
+		// leventshein
+		//		https://stackoverflow.com/a/42287748/1053937
+		// natural
+		// 		https://dzone.com/articles/using-natural-nlp-module
+		// naivebayesclassifier
+		// node-svm
+		// 		http://svmlight.joachims.org/
+
 	})
 	.catch(console.log)
 

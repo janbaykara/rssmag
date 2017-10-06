1ffb4704f843a686ad3e2fb9947768f50f445792
Jan Baykara <jan@baykara.co.uk>
2017-10-06 04:04:04 -0300

    Simple tagging

--- server/www.js
+++ server/www.js
@@ -5,6 +5,8 @@ import KoaRouter from 'koa-router'
 import axios from 'axios'
 import { setupCache } from 'axios-cache-adapter-node'
 import nodeJsonDriver from './nodeJsonDriver'
+import htmlToText from 'html-to-text'
+import autoTagger from 'auto-tagger'
 
 dotenv.config()
 
@@ -20,7 +22,7 @@ const feedlyAPI = axios.create({
 		'Authorization': `OAuth ${process.env.feedlyAccessToken}`
 	},
 	adapter: setupCache({
-		store: new nodeJsonDriver(),
+		store: new nodeJsonDriver({humanReadable: true}),
 	  maxAge: 1000 * 60 * 60 * 24 * 7,
 		debug: true
 	}).adapter
@@ -29,11 +31,36 @@ const feedlyAPI = axios.create({
 // 2. Fetch Feedly data
 // DOCS: https://developer.feedly.com/v3/sandbox/
 feedlyAPI
-	.get('categories')
-	.then(response => {
-		console.log(`API access number __${response.headers['x-ratelimit-count']}__ - ${response.request.fromCache ? 'CACHED' : 'new data'}`)
+.get('categories')
+.then(res => {
+	// console.log(`API access number __${res.headers['x-ratelimit-count']}__ - ${res.request.fromCache ? 'CACHED' : 'new data'}`)
+	console.log(`No of Categories: ${res.data.length}`)
+
+	// From above
+	const streamId = 'user/3a94abfc-0869-47f0-9e7c-892608dd551c/category/Political Comment';
+
+	feedlyAPI
+	.get(`streams/${encodeURIComponent(streamId)}/contents`)
+	.then(res => {
+		// console.log(`API access number __${res.headers['x-ratelimit-count']}__ - ${res.request.fromCache ? 'CACHED' : 'new data'}`)
+		console.log(`No of Entries: ${res.data.items.length}`)
+
+		// Tag articles
+		// Ultimately I want a venn diagram of most-exclusive tags
+		var tags = autoTagger
+			.useStopWords('en')
+			.useStopWords(['http','https','www','com','co','org'])
+
+		res.data.items.forEach(item => {
+			let textBlob = item.title+/*item.keywords+*/(/*item.content ? item.content.content :*/ item.summary ? item.summary.content : '')
+			textBlob = htmlToText.fromString(textBlob);
+			console.log(tags.fromText(textBlob,3,3))
+		})
 	})
 	.catch(console.log)
+
+})
+.catch(console.log)
 
 // 3. Categorise, bundle, format
 // 4. Send to UI for rendering

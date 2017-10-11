e6e315fec7bcc52a113b7c11e4e3f62019890f13
Jan Baykara <jan@baykara.co.uk>
2017-10-11 17:01:32 -0300

    Categories to hideable sidebar

--- client/src/App.js
+++ client/src/App.js
@@ -1,18 +1,25 @@
 import React, { Component } from 'react';
 import {
   BrowserRouter as Router,
-	Redirect,
+  Redirect,
   Route
 } from 'react-router-dom'
+import styled from 'styled-components';
 import 'tachyons';
 import CategoriesNav from './components/CategoriesNav'
 import CategoryView from './components/CategoryView'
 
+const UnselectableHeader = styled.header`
+  user-select: none;
+`
+
 export default class App extends Component {
   constructor({match}) {
-		console.log()
-		super()
-		this.state = {}
+    console.log()
+    super()
+    this.state = {
+      isSidebarOpen: false
+    }
   }
 
   componentDidMount() {
@@ -26,17 +33,29 @@ export default class App extends Component {
     .catch(console.log)
   }
 
+  toggleSidebar = () => {
+    this.setState({isSidebarOpen: !this.state.isSidebarOpen})
+  }
+
   render = () => {
     let defaultCategoryId = encodeURIComponent('user/3a94abfc-0869-47f0-9e7c-892608dd551c/category/Political Comment')
     return (
       <Router>
         <div>
-          <CategoriesNav categories={this.state.categories} />
           <Route exact path='/' render={() => (
             <Redirect to={'/'+defaultCategoryId} />
           )} />
+          <CategoriesNav
+            categories={this.state.categories}
+            isOpen={this.state.isSidebarOpen}
+            toggleSidebar={this.toggleSidebar}
+          />
+          <UnselectableHeader onClick={this.toggleSidebar} className='w-100 w-10-l center ml0-l tc tl-l pa3 pointer'>
+            <div className='fw9 f3'>RSSMAG</div>
+            <div className='i'>Categories</div>
+          </UnselectableHeader>
           <Route path='/:categoryId' render={(...args) => (
-            <CategoryView categories={this.state.categories} {...args} />
+            <main className='w-100 w-80-ns center'><CategoryView categories={this.state.categories} {...args} /></main>
           )} />
         </div>
       </Router>
--- client/src/components/CategoriesNav.js
+++ client/src/components/CategoriesNav.js
@@ -1,23 +1,78 @@
 import React from 'react';
 import { Link } from 'react-router-dom'
+import styled from 'styled-components';
 import 'tachyons';
 
-export default function CategoriesNav({categories, pickCategory}) {
-	return (
-		<header className='w-100 pa3 pb0 mb2'>
-				<span className='fw9 f3 v-mid dib mr1 mb1'>RSSMAG</span>
-				{
-				categories ? categories.map(cat => (
-					<span
-						key={cat.id}
-						className='v-mid fw6 f6 pa2 dib pointer dim bg-black-10 black-80 br2 mr1 mb1'>
-						<Link
-							to={'/'+encodeURIComponent(cat.id)}
-							className='link'
-							title={cat.description}>{cat.label}</Link>
-					</span>
-				)) : <div className='tc moon-gray w-100 f3 fw9'>Loading categories</div>
-				}
-		</header>
-	)
+const slideDuration = '0.15s';
+
+const ModalBackground = styled.div`
+&:before {
+	content: '';
+	background: #000000;
+	position: fixed;
+	top: 0;
+	left: 0;
+	width: 100%;
+	height: 100%;
+	z-index: 1;
+	opacity: 0;
+	transition: opacity ${slideDuration} ease;
+	pointer-events: none;
 }
+
+.sidebarOpen > &:before {
+	opacity: 0.3;
+	pointer-events: inherit;
+}
+`;
+
+const Sidebar = styled.div`
+transition: all ${slideDuration} ease;
+
+> * { opacity: 1; }
+
+.sidebarClosed > & > * { opacity: 0.3; }
+
+.sidebarClosed > & {
+	width: 0;
+	padding-left: 0;
+	padding-right: 0;
+	box-shadow: none;
+}
+`;
+
+const NoOverflowDiv = styled.div`
+white-space: nowrap;
+overflow: hidden;
+`
+
+const UnselectableHeader = styled(NoOverflowDiv)`
+user-select: none;
+`
+
+export default function CategoriesNav({categories, isOpen, toggleSidebar}) {
+	return (
+		<nav className={isOpen ? 'sidebarOpen' : 'sidebarClosed'}>
+			<ModalBackground className='pointer' onClick={toggleSidebar} />
+			<Sidebar className='bg-black-90 near-white z-3 fixed top-0 left-0 h-100 w-90 w-50-ns w-30-l h-100 shadow-2 overflow-y-auto'>
+				<UnselectableHeader
+					className='pa3 pointer'
+					onClick={toggleSidebar}>
+					<div className='fw9 f3'>RSSMAG</div>
+					<div className='i'>Your Feedly categories</div>
+				</UnselectableHeader>
+				{categories ? (<ul class='list pa3'>
+					{categories.map(cat => (
+						<NoOverflowDiv key={cat.id} className='pointer dim mv2'>
+							<Link
+								onClick={toggleSidebar}
+								to={'/'+encodeURIComponent(cat.id)}
+								className='link black-70 fw4 f5 '
+								title={cat.description}>{cat.label}</Link>
+							</NoOverflowDiv>
+						))}
+					</ul>) : <NoOverflowDiv className='moon-gray pa3 w-100 f3 fw9 pv4'>Loading categories</NoOverflowDiv>}
+				</Sidebar>
+			</nav>
+		)
+	}

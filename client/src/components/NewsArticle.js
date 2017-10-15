888267aa71e3e94a7484a2de86cccef812f36a7c
Jan Baykara <jan@baykara.co.uk>
2017-10-15 19:33:58 +0100

    Colour-code bundles, simplify bundle/article classes to functions

--- client/src/components/NewsArticle.js
+++ client/src/components/NewsArticle.js
@@ -1,81 +1,77 @@
-import React, { Component } from 'react';
+import React from 'react';
 import 'tachyons';
 import Middot from './Middot'
 
-export default class NewsArticle extends Component {
-  render() {
-		let { index, article } = this.props
+export default function NewsArticle(props) {
+	let { index, article } = props
 
-		let date = new Date(article.published)
+	let date = new Date(article.published)
 
-    // CSS
-		let conditionalSeparator = this.props.grid ? ' bt b--near-white ' : ' ';
+  // CSS
+	let conditionalSeparator = props.grid ? ' bt b--near-white ' : ' ';
 
-    let layoutType =
-      this.props.column
-        ? this.props.substory
-          ? layoutType = 'substory' : layoutType = 'columnstory'
-        : this.props.grid && this.props.bigstory
-          ? layoutType = 'biggridstory' : layoutType = 'gridstory';
+  let layoutType =
+    props.column
+      ? props.substory
+        ? 'substory' : 'columnstory'
+      : props.grid && props.bigstory
+        ? 'biggridstory' : 'gridstory';
 
-    let conditionalWidth;
-    switch(layoutType) {
-      case 'columnstory': conditionalWidth = ' w-100 ';
-        break;
-      case 'substory': conditionalWidth = ' w-100 w-50-l ';
-        break;
-      case 'biggridstory': conditionalWidth = ' w-100 w-50-ns ';
-        break;
-      case 'gridstory': conditionalWidth = ' w-100 w-50-m w-25-l ';
-        break;
-    }
-
-    // CSS for horizontal [ title | image ]
-    let showImage = index === 0 && article.engagementRate > 1 && article.imageURL;
-    let showSummary = article.summary && ((index === 0 && article.engagementRate > 0.5) || this.props.bigstory);
-    let horizontalLayout = Boolean(
-      this.props.bigstory
-      && (layoutType === 'columnstory' || layoutType === 'biggridstory')
-      && showSummary
-      && showImage
-      && article.engagementRate > 2
-      && (layoutType === 'biggridstory' ? Math.random() > 0.3 : Math.random() > 0.5)
-    )
-    // Randomly decide the order of horizontal [ image | title ] articles
-    let imgOrder = Math.random() > 0.5 ? ' order-1 ' : '';
-
-    return (
-			<article className={'pa1 bg-white '+conditionalWidth+conditionalSeparator}>
-				<a href={article.url} className={'db h-100 link dim pointer bg-near-white black-80 '+(horizontalLayout ? ' flex-l flex-row-l ' : '')}>
-					{showImage && (
-						<div className={'cover cover bg-black-10 mb2 '+(horizontalLayout ? ' w-50-l h-100-l '+imgOrder : ' w-100 h5 ')}
-              style={{backgroundImage: `url(${article.imageURL})`}} />
-					)}
-					<div className={'pa2 '+(horizontalLayout ? ' w-50-l ' : ' w-100 ')}>
-						{(index === 0 && article.engagementRate > 5) ? (
-							<div className='fw9 f2 mb2'><span className='orange'>{article.engagementRate}</span> {article.title}</div>
-						)
-						:
-						(index === 0 && article.engagementRate > 1) ? (
-							<div className='fw4 f3 mb2'><span className='orange'>{article.engagementRate}</span> {article.title}</div>
-						)
-						:
-						(index > 0 && article.engagementRate > 1) ? (
-							<div className='fw5 f4 lh-title'><span className='orange'>{article.engagementRate}</span> {article.title}</div>
-						)
-						:
-						(
-							<div className='fw5 f5 lh-title'><span className='orange'>{article.engagementRate}</span> {article.title}</div>
-						)}
-						<div className='fw6 f7 black-30 mt1 lh-copy'>
-							{article.source && <Middot className='black-40 bg-white ph1 br2 dib'>{article.source}</Middot>}
-							{article.engagementRate > 1 && article.author && <Middot>{article.author}</Middot>}
-							{article.published && <Middot>{date.toLocaleDateString('en-GB')}</Middot>}
-						</div>
-						{showSummary && <p className='baskerville black-60 lh-title mw6' style={{wordWrap: 'break-word'}}>{article.summary}</p>}
-					</div>
-				</a>
-			</article>
-    );
+  let conditionalWidth;
+  switch(layoutType) {
+    case 'columnstory': conditionalWidth = ' w-100 ';
+      break;
+    case 'substory': conditionalWidth = ' w-100 w-50-l ';
+      break;
+    case 'biggridstory': conditionalWidth = ' w-100 w-50-ns ';
+      break;
+    case 'gridstory': conditionalWidth = ' w-100 w-50-m w-25-l ';
+      break;
+    default:
+      conditionalWidth = ' w-100 '
   }
+
+  // CSS for horizontal [ title | image ]
+  let showImage = index === 0 && article.engagementRate > 1 && article.imageURL;
+  let showSummary = article.summary && ((index === 0 && article.engagementRate > 0.5) || props.bigstory);
+  let horizontalLayout = Boolean(
+    props.bigstory
+    && (layoutType === 'columnstory' || layoutType === 'biggridstory')
+    && showSummary
+    && showImage
+    && article.engagementRate > 2
+    && (layoutType === 'biggridstory' ? Math.random() > 0.3 : Math.random() > 0.5)
+  )
+  // Randomly decide the order of horizontal [ image | title ] articles
+  let imgOrder = Math.random() > 0.5 ? ' order-1 ' : '';
+
+  let titleClasses = (index === 0 && article.engagementRate > 5) ? 'fw9 f2 mb2'
+                  : (index === 0 && article.engagementRate > 1) ? 'fw4 f3 mb2'
+                  : (index > 0 && article.engagementRate > 1) ? 'fw5 f4 lh-title'
+                  : 'fw5 f5 lh-title';
+
+  return (
+		<article className={'pa1 bg-white '+conditionalWidth+conditionalSeparator}>
+			<a href={article.url} className={'overflow-hidden bl db h-100 link dim pointer bg-near-white black-80 '+(horizontalLayout ? ' flex-l flex-row-l ' : '')}
+        style={{borderColor: props.themeColour}}>
+				{showImage && (
+					<div className={'cover cover bg-black-10 mb2 '+(horizontalLayout ? ' w-50-l h-100-l '+imgOrder : ' w-100 h5 ')}
+            style={{backgroundImage: `url(${article.imageURL})`}} />
+				)}
+				<div className={'pa2 '+(horizontalLayout ? ' w-50-l ' : ' w-100 ')}>
+					<header className={titleClasses}>
+            <span className='orange'>{article.engagementRate}</span>
+            <span className='black-10 fw1'>/</span>
+            <span>{article.title}</span>
+          </header>
+					<div className='fw6 f7 black-30 mt1 lh-copy'>
+						{article.source && <Middot className='black-40 bg-white ph1 br2 dib'>{article.source}</Middot>}
+						{article.engagementRate > 1 && article.author && <Middot>{article.author}</Middot>}
+						{article.published && <Middot>{date.toLocaleDateString('en-GB')}</Middot>}
+					</div>
+					{showSummary && <p className='baskerville black-60 lh-title mw6' style={{wordWrap: 'break-word'}}>{article.summary}</p>}
+				</div>
+			</a>
+		</article>
+  );
 }
--- client/src/components/NewsBundle.js
+++ client/src/components/NewsBundle.js
@@ -1,43 +1,50 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import 'tachyons';
-import NewsArticle from './NewsArticle'
+import NewsArticle from './NewsArticle';
+import ColorHash from 'color-hash';
 
-export default class NewsBundle extends Component {
-  render() {
-    return (
-      <section className='h-100 bg-white' style={{outline: '1px solid #EEE'}}>
-        {this.props.name ? (
-          // Big header
-          <header className='w-100 ph3 pb3 pt4 tc fw3 f3 black-50 lh-solid'>{this.props.name}</header>
-        ) : this.props.bundle.name ? (
-          // Little header
-          <header className='w-100 ph3 pt3'>
-            <Link to={'/topic/'+encodeURIComponent(this.props.bundle.name)} className='link ttu fw9 f7 black-30'>{this.props.bundle.name}</Link>
-          </header>
-          // Or nothing
-        ) : false}
-				<div className='pa1 flex flex-wrap'>
-					{this.props.bundle.articles.map((article,i) => (
-            <NewsArticle key={i} index={i} article={article}
-              column={this.props.column}
-              grid={this.props.grid}
-              bigstory={Boolean((
-                this.props.feature
-                && i === 0
-              ) || (
-                this.props.grid
-                && article.engagementRate > 1
-                && (article.imageURL || article.summary)
-              ))}
-              substory={Boolean(
-                this.props.feature
-                && article.engagementRate <= 5
-                && !((article.imageURL || article.summary) && i == 0)
-              )} />
-					))}
-				</div>
-			</section>
-    );
-  }
+const colorHash = new ColorHash({
+  saturation: 1,
+  lightness: 0.4
+});
+
+export default function NewsBundle(props) {
+  let bundleColour = props.bundle.name === '__unbundled' ? 'rgba(0,0,0,0.2)' : colorHash.hex(props.bundle.name);
+
+  return (
+    <section className='h-100 bg-white' style={{outline: '1px solid #EEE'}}>
+      {props.name ? (
+        // Big header
+        <header className='w-100 ph3 pb3 pt4 tc fw3 f3 black-50 lh-solid'>{props.name}</header>
+      ) : props.bundle.name ? (
+        // Little header
+        <header className='w-100 ph3 pt3'>
+          <Link to={'/topic/'+encodeURIComponent(props.bundle.name)} className='link ttu fw9 f7 black-30'>{props.bundle.name}</Link>
+        </header>
+        // Or nothing
+      ) : false}
+			<div className='pa1 flex flex-wrap'>
+				{props.bundle.articles.map((article,i) => (
+          <NewsArticle key={i} index={i} article={article}
+            themeColour={bundleColour}
+            column={props.column}
+            grid={props.grid}
+            bigstory={Boolean((
+              props.feature
+              && i === 0
+            ) || (
+              props.grid
+              && article.engagementRate > 1
+              && (article.imageURL || article.summary)
+            ))}
+            substory={Boolean(
+              props.feature
+              && article.engagementRate <= 5
+              && !((article.imageURL || article.summary) && i === 0)
+            )} />
+				))}
+			</div>
+		</section>
+  );
 }
